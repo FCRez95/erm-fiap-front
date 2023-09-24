13d8fc7caeef30af00eacd718022bb41925ad700
Fabio Rezende <fcrez95@gmail.com>
2023-09-23 22:48:42 -0300

    fix: correct bugs

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -6,7 +6,6 @@ import LoginPage from '../pages/Login'
 //import MetricsPage from '../pages/Metricas'
 import CreateClickPage from '../pages/CreateClick'
 import CampaingsPage from '../pages/Campaings'
-import CampaingPage from '../pages/Campaing'
 
 interface Props {
 	user?: User []
@@ -20,7 +19,6 @@ const AppRoutes = ({ user }: Props) => {
 				<Route path="/" element={<Layout user={user}/>}>
           			<Route path='login' element={<LoginPage/>} />
           			<Route path='campaings' element={<CampaingsPage user={user}/>} />
-          			<Route path='campaings/:idCampaing' element={<CampaingPage user={user}/>} />
 					<Route path='*' element={<LoginPage />} />
 				</Route>
 			</Routes>
